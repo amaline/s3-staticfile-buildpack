47f6bf56b59c88ee3f664ab4c54a23acc0eb2583
Al Maline <amaline@users.noreply.github.com>
2016-10-19 16:06:21 -0400

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -39,7 +39,9 @@ then
    AWS_SECRET=`echo $VCAP_SERVICES|jq .s3[].credentials.secret_access_key`
    echo "AWS_SECRET=${AWS_SECRET}"
    
-   export AWS_SIGNING_KEY=`$APP_ROOT/generate_signing_key -k $AWS_SECRET -r $AWS_REGION -s s3 -d $CURRENT_DATE`
+   $APP_ROOT/generate_signing_key -k $AWS_SECRET -r $AWS_REGION -s s3 -d $CURRENT_DATE -v
+   
+   export AWS_SIGNING_KEY=`$APP_ROOT/generate_signing_key -k $AWS_SECRET -r $AWS_REGION -s s3 -d $CURRENT_DATE|head -1`
    
    echo "AWS_SIGNING_KEY=${AWS_SIGNING_KEY}"
 fi
