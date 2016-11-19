85044e7fa0af42e549824ed76fd7964e18bef237
Al Maline <amaline@yahoo.com>
2016-11-19 18:25:26 +0000

    attempting s3fs mount of bucket. added debug output to s3fs

--- bin/boot.sh
+++ bin/boot.sh
@@ -50,9 +50,8 @@ then
    echo "${AWS_ACCESS_KEY}:${AWS_SECRET}" > $APP_ROOT/s3cred
    chmod 600 $APP_ROOT/s3cred
    mkdir $APP_ROOT/webdav
-   $APP_ROOT/s3fs ${AWS_S3_BUCKET} $APP_ROOT/webdav -o passwd_file=${APP_ROOT}/s3cred
-   ls $APP_ROOT
-   ls $APP_ROOT/webdav
+   $APP_ROOT/s3fs ${AWS_S3_BUCKET} $APP_ROOT/webdav -o passwd_file=${APP_ROOT}/s3cred -d -d -f -o f2 -o curldbg
+   echo '--s3fs setup complete--'
 fi
 
 if [ -f $APP_ROOT/nginx/conf/.enable_cached_dirs ]
