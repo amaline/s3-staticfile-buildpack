b425a164d864d3701aee6befa155df0c9d4873bf
Al Maline <amaline@yahoo.com>
2016-11-28 15:24:45 +0000

    added -f option to goofys so it will not use syslog; outlog to fifo named pipe

--- bin/upload.sh
+++ bin/upload.sh
@@ -12,6 +12,7 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: ./upload zipfile"
     exit 100
 fi
+echo "APP_ROOT=${APP_ROOT}"
 
 AWS_ACCESS_KEY=`echo $VCAP_SERVICES|jq --raw-output .s3[].credentials.access_key_id`
 AWS_S3_BUCKET=`echo $VCAP_SERVICES|jq --raw-output  .s3[].credentials.bucket`
@@ -29,14 +30,29 @@ chmod 600 ~/.aws/credentials
 
 #echo "${AWS_ACCESS_KEY}:${AWS_SECRET}" > s3cred
 
-mkdir tmpupload
+mkdir $APP_ROOT/goofys
+LOGDIR=$APP_ROOT/goofys/logs
+mkdir $LOGDIR
+TMPUPLOAD=$APP_ROOT/goofys/tmpupload
+mkdir $TMPUPLOAD
+
+mkfifo $LOGDIR/out.log
+cat < $LOGDIR/out.log &
+LOGCAT_PID=$!
 
 #./s3fs $AWS_S3_BUCKET ./tmpupload -o passwd_file=s3cred
-UID=$(id -u)
-GID=$(id -g)
-./goofys --uid ${UID} --gid ${GID} $AWS_S3_BUCKET ./tmpupload
+MYUID=$(id -u)
+MYGID=$(id -g)
+./goofys -f --uid ${MYUID} --gid ${MYGID} $AWS_S3_BUCKET $TMPUPLOAD 2>&1 > $LOGDIR/out.log &
+GOOFYS_PID=$!
 
-unzip -fo $1 -d tmpupload
-ls -lR tmpupload
-rm s3cred
-sudo umount ./tmpupload
+unzip -fo $1 -d $TMPUPLOAD
+ls -lR $TMPUPLOAD > $LOGDIR/uploadinfo.log
+cat $LOGDIR/uploadinfo.log
+#rm s3cred
+
+#clean up
+sudo umount $TMPUPLOAD
+rm ~/.aws/credentials
+kill $GOOFYS_PID
+kill $LOGCAT_PID
